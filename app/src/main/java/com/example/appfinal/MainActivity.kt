400c5108c9df6ce653ec06585b68b05a2f9244d7
kevinRepos-droid <acelite27@gmail.com>
2021-09-07 02:11:36 -0600

    agregue mas clases como productoactivity y productos adapter

--- app/src/main/java/com/example/appfinal/MainActivity.kt
+++ app/src/main/java/com/example/appfinal/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.appfinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val producto = Producto("Libreta", 100.0, "libreta taquigrafica")
+        val producto2 = Producto("Pegamento", 1000.0, "Pegamento Blanco Smarty")
+
+        val listaProductos = listOf(producto, producto2)
+
+        val adapter = ProductosAdapter(this, listaProductos)
+
+        lista.adapter = adapter
+
+        lista.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, ProductoActivity::class.java)
+            intent.putExtra("producto", listaProductos[position])
+            startActivity(intent)
+
+
+        }
     }
 }
